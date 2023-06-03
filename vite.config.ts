1decf64c09c40c9eab4ae8d773421bece29bf47d
zhoululu <zhoululu@aegis-data.cn>
2023-06-03 20:52:44 +0800

    fix: 精简输出文件格式，只输出umd和es

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, './src/index.ts'),
-      name: 'ztlsl',
-      formats: ['cjs', 'es', 'umd'],
-      fileName: (format: string) => {
-        if(format === 'cjs') return 'ztlsl.cjs'
-        if(format === 'es') return 'ztlsl.mjs'
-        return 'ztlsl.js'
-      }
+      name: 'ztlsl'
     }
   }
 })
